ce9ecd92afdab11393bf14b778477ac6c7075322
Lovepreet Kaur <747018@student.tor.loc>
2019-06-26 14:59:08 -0400

    2:50 commit

---
+++ javascriptfile1.js
@@ -0,0 +1,11 @@
+function waitingFn(timeInMs){
+   const futureTime = Date.now() + timeInMs;
+   while(futureTime>Date.now()){
+    //waiting
+   } 
+
+}
+debugger;
+waitingFn(3000);
+
+console.log("Function called has just ended");
